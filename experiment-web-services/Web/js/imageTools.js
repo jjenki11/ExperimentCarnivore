3683b60e82817787355a71d488ca3a07cbfb9f36
jjenki11 <jjenki11@gmu.edu>
2015-02-05 17:24:21 -0500

    some mods

---
+++ experiment-web-services/Web/js/imageTools.js
@@ -0,0 +1,309 @@
+
+
+var imageTools = function (){	
+
+	var m = new mathPack();
+
+  function set3DWindow(canvas)
+  {
+      var black = new Pre3d.RGBA(0, 0, 0, 1);
+      var white = new Pre3d.RGBA(1, 1, 1, 1);
+
+      var screen_canvas = document.getElementById('ThreeDWin');
+      var renderer = new Pre3d.Renderer(screen_canvas);
+
+      var cubes = [ ];
+
+      for (var i = 0; i < 10; ++i) {
+        for (var j = 0; j < 10; ++j) {
+          for (var k = 0; k < 10; ++k) {
+            if (i == 0 || j == 0 || k == 0 ||
+                i == 9 || j == 9 || k == 9) {
+              var cube = Pre3d.ShapeUtils.makeCube(0.5);
+              var transform = new Pre3d.Transform();
+              transform.translate(i - 5, j - 5, k - 5);
+              cubes.push({
+                shape: cube,
+                color: new Pre3d.RGBA(i / 10, j / 10, k / 10, 0.3),
+                trans: transform});
+            }
+          }
+        }
+      }
+
+      var num_cubes = cubes.length;
+      var cur_white = false;  // Default to black background.
+
+      function draw() {
+        for (var i = 0; i < num_cubes; ++i) {
+          var cube = cubes[i];
+          renderer.fill_rgba = cube.color;
+          renderer.transform = cube.trans;
+          renderer.bufferShape(cube.shape);
+        }
+
+        if (cur_white) {
+          renderer.ctx.setFillColor(1, 1, 1, 1);
+        } else {
+          renderer.ctx.setFillColor(0, 0, 0, 1);
+        }
+        renderer.drawBackground();
+
+        renderer.drawBuffer();
+        renderer.emptyBuffer();
+      }
+
+      renderer.camera.focal_length = 2.5;
+      // Have the engine handle mouse / camera movement for us.
+      DemoUtils.autoCamera(renderer, 0, 0, -30, 0.40, -1.06, 0, draw);
+
+      document.addEventListener('keydown', function(e) {
+        if (e.keyCode != 84)  // t
+          return;
+
+        if (cur_white) {
+          document.body.className = "black";
+        } else {
+          document.body.className = "white";
+        }
+        cur_white = !cur_white;
+        draw();
+      }, false);
+
+      draw();
+      return true;
+    
+  };
+  
+  function load3DCanvas()
+  {
+        var canvas = document.createElement('ThreeDWin');
+        div = document.getElementById('ThreeDWin'); 
+        canvas.id     = "CursorLayer";
+        canvas.width  = 1224;
+        canvas.height = 768;
+        canvas.style.zIndex   = 8;
+        canvas.style.position = "absolute";
+        canvas.style.border   = "1px solid";
+        div.appendChild(canvas)
+  };
+
+	function loadCanvas(id) 
+	{
+        var canvas = document.createElement('canvas');
+        div = document.getElementById(id); 
+        canvas.id     = "CursorLayer";
+        canvas.width  = 1224;
+        canvas.height = 768;
+        canvas.style.zIndex   = 8;
+        canvas.style.position = "absolute";
+        canvas.style.border   = "1px solid";
+        div.appendChild(canvas)
+    };
+
+	function setImage(canvas, url)
+	{
+		var context = canvas.getContext('2d');
+		base_image = new Image();
+		base_image.src = url;
+		base_image.onload = function(){
+			context.drawImage(base_image, 0, 0);
+		}
+	};
+// TBD
+	function setPixel(canvas, i, j, r, g, b)
+	{
+		var c=document.getElementById('imagery');
+		var ctx=c.getContext("2d");
+		var img=document.getElementById(imageHandle);
+		ctx.drawImage(img,0,0);
+		var imgData=ctx.getImageData(0,0,c.width,c.height);
+		// get pixel linear index
+		var linIndex = ((i+1)*4) * (j+1);
+		imgData.data[linIndex] = r;
+		imgData.data[linIndex+1] = g;
+		imgData.data[linIndex+2] = b;
+		imgData.data[linIndex+3] = g;
+		ctx.putImageData(imgData,0,0);		
+	};
+//TBD - FIX
+	function getRows(imageHandle)
+	{
+        var c=document.getElementById('imagery');
+        var ctx=c.getContext("2d");
+        var rows = []
+        var values = [];
+        var imageWidth = c.width;
+        var imageHeight = c.height;
+        var imgData=ctx.getImageData(0,0,c.width,c.height);
+        for(var y = 0; y < imageWidth; y++) {
+          // loop through each row
+          for(var x = 0; x < imageHeight; x++) {
+          	if(x != 0)
+          	{
+          		var red = imgData.data[((imageHeight * y) + x) * 4];
+            	var green = imgData.data[((imageHeight * y) + x) * 4 + 1];
+            	var blue = imgData.data[((imageHeight * y) + x) * 4 + 2];
+	            var alpha = imgData.data[((imageHeight * y) + x) * 4 + 3];
+          		values.push(red);
+          		values.push(green);
+          		values.push(blue);
+          		//values.push(alpha);
+          	}
+          }
+          rows.push(values);
+          values = [];
+        }
+        return rows;
+	};
+// TBD - FIX
+	function getColumns(imageHandle)
+	{
+        var c=document.getElementById('imagery');
+		var ctx=c.getContext("2d");
+        var cols = [];
+        var values = [];
+        var imageWidth = c.width;
+        var imageHeight = c.height;
+        var imgData=ctx.getImageData(0,0,c.width,c.height);
+        for(var y = 0; y < imageHeight; y++) {
+          // loop through each column
+          for(var x = 0; x < imageWidth; x++) {
+          	if(x == 0)
+          	{
+          		var red = imgData.data[((imageWidth * y) + x) * 4];
+            	var green = imgData.data[((imageWidth * y) + x) * 4 + 1];
+            	var blue = imgData.data[((imageWidth * y) + x) * 4 + 2];
+	            var alpha = imgData.data[((imageWidth * y) + x) * 4 + 3];
+          		values.push(red);
+          		values.push(green);
+          		values.push(blue);
+          		//values.push(alpha);
+          	}
+          }
+          cols.push(values);
+          values = [];
+        }
+        return cols;
+	};
+
+	function invert(imageHandle)
+	{
+		var c=document.getElementById('imagery');
+		var ctx=c.getContext("2d");
+		var img=document.getElementById(imageHandle);
+		ctx.drawImage(img,0,0);
+		var imgData=ctx.getImageData(0,0,c.width,c.height);
+		// invert colors
+		for (var i=0;i<imgData.data.length;i+=4)
+		{
+			imgData.data[i]=255-imgData.data[i];
+			imgData.data[i+1]=255-imgData.data[i+1];
+			imgData.data[i+2]=255-imgData.data[i+2];
+			imgData.data[i+3]=255;
+		}
+		ctx.putImageData(imgData,0,0);
+	};
+
+	function gray(imageHandle)
+	{
+		var c=document.getElementById('imagery');
+		var ctx=c.getContext("2d");
+		var img=document.getElementById(imageHandle);
+		ctx.drawImage(img,0,0);
+		var imgData=ctx.getImageData(0,0,c.width,c.height);
+		// average rgb and set r,g,b each to average
+		for (var i=0;i<imgData.data.length;i+=4)
+		{
+			var a = m.average([imgData.data[i],imgData.data[i+1],imgData.data[i+2]]);
+			imgData.data[i]= a;
+			imgData.data[i+1]= a;
+			imgData.data[i+2]= a;
+			imgData.data[i+3]= 255;
+		}
+		ctx.putImageData(imgData,0,0);
+	};
+
+	function binary(imageHandle, thresh)
+	{
+		var c=document.getElementById('imagery');
+		var ctx=c.getContext("2d");
+		var img=document.getElementById(imageHandle);
+		ctx.drawImage(img,0,0);
+		var imgData=ctx.getImageData(0,0,c.width,c.height);
+		// binarize image
+		for (var i=0;i<imgData.data.length;i+=4)
+		{
+			var a = m.average([imgData.data[i],imgData.data[i+1],imgData.data[i+2]]);
+			if(a >= (thresh || 127))
+			{
+				a = 255;
+			}
+			else
+			{
+				a = 0;
+			}
+			imgData.data[i]= a;
+			imgData.data[i+1]= a;
+			imgData.data[i+2]= a;
+			imgData.data[i+3]= 255;
+		}
+		ctx.putImageData(imgData,0,0);
+	};
+	
+	function hist(imageHandle, direction)
+	{
+		var data = [];
+		var ret = [];
+		if(direction == 'vertical')
+		{
+			data = getColumns(imageHandle);
+		}
+		else	//direction = horizontal
+		{
+			data = getRows(imageHandle);
+		}
+		for(var i = 0; i < data.length; i++)
+		{
+			ret.push([(i+1), (m.sum(data[i]))]);
+		}
+		return ret;
+	};
+	
+    /**
+     *    Returns array of keys from a map
+     */    
+	var keyList = function(obj)
+    {
+        var k = [];
+        for(var name in obj)
+        { 
+            k.push(name); 
+        }
+        return k;
+    };
+    /**
+     *    Returns array of values from a map
+     */    
+    var valueList = function(obj)
+    {
+        var v = [];
+        for(var name in obj)
+        { 
+            v.push(obj[name]); 
+        }
+        return v;    
+    };
+	
+	return {
+		invert: invert,
+		setImage: setImage,
+		loadCanvas: loadCanvas,
+		load3DCanvas: load3DCanvas,
+		set3DWindow: set3DWindow,
+		gray: gray,
+		binary: binary,
+		hist: hist,
+	};
+
+};
---
+++ experiment-web-services/Web/js/pluginHandler.js
@@ -0,0 +1,452 @@
+var pluginHandler = function(){
+
+	var hashMap = {}; // for variables
+	var funcMap = {}; // for functions
+
+	var mathUtils = new MathOps();
+	//var pageObj = {index: {nav: 0, mid: 0, low: 0}};
+
+/* THIS is where we do all the setting of page sections to 
+	give the illusion of only 1 url and fast performance!
+*/
+	function setTop(A)
+	{
+		$("#nav").html(A).draggable();
+	};
+
+	function setMid(A)
+	{
+		$("#mid").html(A).draggable();
+	};
+
+	function setLow(A)
+	{
+		$("#low").html(A).draggable();
+	};
+
+	function setImage(A)
+	{
+		$("#image").html(A).draggable();
+	};
+	
+	function setConsole(A)
+	{
+		$("#console").html(A);
+	};
+	
+	function setVarExplorer(A)
+	{
+		$("#vars").html(A);
+	};
+	
+	function setOutput(A)
+	{
+		$("#out").html(A);
+	};
+	
+	function setPass(A)
+	{
+	  $("#pass").html(A);
+	};
+	
+	function returnArray(inp)
+	{
+		return mathUtils.vectorN(hashMap.getItem(inp));
+	};
+
+	function retVariableFromMap(inp)
+	{
+		var txt = "";
+		txt +=inp;
+		var x = hashMap.getItem(""+inp);
+		console.log("Key: (" + inp + "), Val: ("+x+")");
+		if(!x){return ;}
+		if(x.length > 1){
+			if(x[0].length > 0){
+				return mathUtils.matN(x, x.length, x[0].length);
+			}
+			else {
+				return mathUtils.vectorN(x);
+			}
+		}
+		return parseFloat(hashMap.getItem(txt));
+	}
+
+	function setMap(Hm)
+	{
+		hashMap = Hm;
+	};
+
+	function clearMap()
+	{
+		hashMap.clear();
+	};	
+
+	function makeHashMap()
+	{
+		setMap(new HashTable());
+	};
+
+	function functionHandler(data)
+	{
+		return data.split('(')[0];
+	};
+
+	function submitHandle()
+	{
+		var input=document.getElementById("console").value.replace(/\s/g, '');		
+		console.log(input);
+				console.log(input);
+						console.log(input);
+		$("#vars").val("");
+
+		var vec = input.split('{');
+
+		var fun = input.split('\(');
+		var eql = input.split('=');
+		var add = input.split('+');
+		var sub = input.split('-');
+		var mul = input.split('*');
+		var div = input.split('/');
+
+		var v1 = 0;
+		var v2 = 0;
+		var opt = "";
+		var outcome = 0;
+
+		if(fun.length == 2){
+			v1 = fun[0];
+			v2 = fun[1].replace(/\)/g, '');
+			console.log("V1: "+v1);
+			console.log("V2: "+retVariableFromMap(v2));
+
+			if(v1 === 'avg'){
+				console.log("AVG: "+mathUtils.averageVec(retVariableFromMap(v2)));
+			} else if(v1 === 'image'){
+				console.log("SHOWING IMAGE: "+retVariableFromMap(v2));
+				makeImageWindow(retVariableFromMap(v2));
+			} else if(v1 === 'eye'){
+				var s = mathUtils.eye(parseInt(v2));
+				mathUtils.printMatrix(s);
+			} else {
+				// unknown
+			}
+		}
+
+		if(vec.length == 2){
+
+			var x = vec[1].substring(0, (vec[1].length - 1));
+			var y = x.split(',');
+			console.log(x);
+
+			mathUtils.printVectorN(x);
+			hashMap.setItem(vec[0], mathUtils.vectorN(y));
+			//mathUtils.printMatrix(I);
+			//console.log("AVERAGE: "+aI);
+
+			//console.log(mathUtils.printVectorN(mathUtils.vectorN(vec[1].split(','))));
+		}
+
+		if(eql.length == 2){ // we found equals
+			v1 = eql[0];
+			v2 = eql[1];//.replace(/=/g, '');
+			opt = " <- "; //fancy notation to assign
+			outcome = " (variable assignment)";
+			hashMap.setItem(eql[0], v2[1]);
+		} else if(add.length == 2){
+			v1 = retVariableFromMap(add[0]);
+			v2 = retVariableFromMap(add[1]);
+			opt = '+';
+			outcome = v1 + v2;
+		} else if(sub.length == 2){
+			v1 = retVariableFromMap(sub[0]);
+			v2 = retVariableFromMap(sub[1]);
+			opt = '-';
+			outcome = v1 - v2;
+		} else if(mul.length == 2){
+			v1 = retVariableFromMap(mul[0]);
+			v2 = retVariableFromMap(mul[1]);
+			opt = '*';
+			outcome = v1 * v2;
+		} else if(div.length == 2){
+			v1 = retVariableFromMap(div[0]);
+			v2 = retVariableFromMap(div[1]);
+			opt = '/';
+			outcome = v1 / v2;
+		} else {
+			/*
+			if(fun.length ==2){
+				var x = fun[1].split(')')[0];
+				//console.log(returnArray(x));
+				//console.log("AVG OF "+mathUtils.printVectorN(mathUtils.vectorN(x))+ " = " + mathUtils.averageVec(mathUtils.vectorN(x)));
+			}*/
+			if(input === 'clear'){
+				makeHashMap();
+				document.getElementById("out").value = "ready...";
+			} else if(input === 'erase'){
+				clearImage();
+			} else if(input === 'plot'){
+				updateOutputConsole("TBD implement plot...");
+				return;
+			} else {
+				console.log("YOU DID NOT HAVE ENTER IN TEXT");
+				document.getElementById("out").value = (input[0]+ " = "+retVariableFromMap(input[0]));
+			}	
+		} 
+		updateVariableEditor();
+		updateOutputConsole(v1+" "+opt+" "+v2+" = "+outcome);
+	};	
+
+	function makeInputWindow()
+	{
+		var txt = "<div id=\"console\" class=\"ui-widget-content\">Console Window";
+			txt+="<textarea id=\"console\" onBlur=\"javascript:if(this.value==''){this.value=this.defaultValue;}\" onFocus=\"javascript:if(this.value==this.defaultValue){this.value='';}\"></textarea></div>";
+			txt+="</div>";	
+			//	This shit basically parses the input and does basic calculator stuff		
+			    //	$("#submitConsole").bind('click', submitConsole);			
+			setConsole(txt);
+	};
+	
+	function clearWindows()
+	{
+	  $("#out").val("");
+	  $("#console").val("");
+	};
+	
+	function makePasswordEntry()
+	{
+	  var txt = "<div id=\"pass\">Enter Password:";
+		    txt+="<input id=\"passInput\"></input><button id=\"enterPass\">Enter</button></div>";
+		setPass(txt);
+	};
+
+	function makeVariableEditor()
+	{
+		var txt = "<div>Variable Editor";
+			txt+="<textarea id=\"vars\" value=\"YO\">Message</textarea></div>";
+			makeHashMap();
+			setVarExplorer(txt);
+	};
+
+	function makeOutputTerminal()
+	{
+		var txt = "<div>Output Window";
+			txt+="<textarea id=\"out\"></textarea></div>";
+			setOutput(txt);
+	};
+
+	function makeImage()
+	{
+		var txt = "<canvas id=\"imagery\"><img src=\"images/scream.jpg\" alt=\"scream\" style=\"width:250px;height:250px;visibility:hidden;\"></canvas>";
+		return txt;
+	};
+
+
+	function convertBMPToImageData(data) {
+		var Width = data.width;
+		var Height = data.height;
+		var data = bmp.data;
+		var bmpdata = data.data;
+		var stride = data.stride;
+
+		for (var y = 0; y < Height; ++y) {
+ 			for (var x = 0; x < Width; ++x) {
+  				var index1 = (x+Width*(Height-y))*4;
+    			var index2 = x * 3 + stride * y;
+    			data[index1] = bmpdata[index2 + 2];
+  				data[index1 + 1] = bmpdata[index2 + 1];
+    			data[index1 + 2] = bmpdata[index2];
+    			data[index1 + 3] = 255;  	// the alpha channel
+   			}
+  		}
+  		console.log(data.length);
+  		return data;
+  	};
+
+/*	tabling until we decide on image rep'n */
+  	function viewImageMatrix(data)
+  	{	
+  		convertBMPToImageData(data)
+  	};
+
+  	function drawImage(data)
+  	{
+		if(!data)
+			data = "images/scream.bmp";  	
+
+		var img = new Image();
+		img.src = data;
+  		var can = document.getElementById('canvas1');
+  		var con = can.getContext('2d');
+  		var imgData = con.getImageData(0, 0, can.height, can.width);
+  		try{
+ 		    con.putImageData(imgData, 0, 0);
+		    con.drawImage(img, 0, 0);
+		  } catch(err) {
+		    //something to handle the issue
+		    data = "images/scream.bmp"
+		    con.putImageData(imgData, 0, 0);
+		    con.drawImage(img, 0, 0);
+		  }
+  	};
+
+  	function clearImage(){
+  		var can = document.getElementById('canvas1');
+  		can.getContext('2d').clearRect(0, 0, can.width, can.height);
+  		can.getContext('2d').clearRect ( x , y , w , h );
+  	};
+
+	function makeImageWindow(data)
+	{		
+ 		drawImage(data);				
+	};
+
+	function paintCanvas()
+	{
+		setImage("<canvas id=\"canvas1\" width=\"512\" height=\"512\"> Fail ... </canvas>");
+	}
+
+	function updateInputWindow()
+	{
+		document.getElementById("console").value = " > ";
+	};
+
+	function updateVariableEditor()
+	{
+		var txt = "";
+		for(var k in hashMap.items){
+			txt += k + "    " + hashMap.getItem(k) + "\n";
+		}
+		document.getElementById("vars").value = (txt);
+	};
+
+	function updateOutputConsole(txt)
+	{
+		document.getElementById("out").value = txt;
+	};
+	
+	function updatePlotWindow(data)
+	{
+	  $("#centerContent").html("<div id=\"content\"><div class=\"demo-container\"><div id=\"placeholder\" class=\"demo-placeholder\"></div></div></div>");
+	  $.plot("#placeholder", [ data ] );
+	};
+	
+	function updateMultiPlotWindow(data)
+	{
+	  $("#centerContent").html("<div id=\"content\"><div class=\"demo-container\"><div id=\"placeholder\" class=\"demo-placeholder\"></div></div></div>");
+	  
+	  // parse data request into mgmt system 
+	  
+	  
+	  
+	  $.plot("#placeholder", [ data ] );
+	  
+	  
+	};
+	
+	function hideByHandle(handle)
+	{
+	  $(handle).hide();
+	};
+	
+	function hideList(list)
+	{
+	  for(var idx in list)
+	  {
+	    hideByHandle(list[idx]);
+	  }
+	};
+	
+	function showByHandle(handle)
+	{
+	  return $(handle).show();
+	};
+	
+	function showList(list)
+	{
+	  var l = [];
+	  for(var idx in list)
+	  {
+	    l.push(showByHandle(list[idx]));
+	  }
+	  return l;
+	};
+	
+	function setContent(dest, content)
+	{
+	  $(dest).html($(content));
+	  showByHandle(content);
+	};
+	
+	function appendToHandle(dest, content)
+	{
+	  $(dest).append(showList(content));
+	};
+
+  function createDroppable(dest, accepter, dropper)
+  {
+    $(dest).droppable({
+			accept: accepter,
+			drop:   dropper	
+		});
+  };
+  
+  function createDraggableItems(list)
+  {
+    for(var idx in list)
+    {
+      $(list[idx]).draggable({
+			  helper:	function () { return $(this).clone().appendTo('body').css('zIndex',5).show(); },
+			  cursor:	'move'
+		  });
+    }    
+  };
+  
+  function makeArrayFromString(str)
+  {
+    var array = [];
+    var sp = str.split(',');
+    for(var idx in sp)
+    {
+      var tr = sp[idx].replace(/\s/g, '');  //get rid of spaces grr
+      array.push(tr);
+    }
+    return array;  
+  };
+  
+  function createNumberLine(start, upperBound)
+  {
+    var len = upperBound - start;
+    var i = 0;
+    var numLine = [];
+    while(i<=len){
+      numLine.push(start+i);
+      i++;
+    }
+    return numLine;    
+  };
+  
+	return {
+		makeInputWindow: makeInputWindow,
+		makeVariableEditor:makeVariableEditor,
+		makeOutputTerminal:makeOutputTerminal,
+		makeImageWindow:makeImageWindow,
+		makePasswordEntry:makePasswordEntry,
+		paintCanvas:paintCanvas,
+		updateVariableEditor:updateVariableEditor,
+		updateInputWindow:updateInputWindow,
+		updateOutputConsole:updateOutputConsole,
+		clearWindows:clearWindows,
+		makeImage:makeImage,
+		submitHandle:submitHandle,
+		updatePlotWindow:updatePlotWindow,
+		hideList:hideList,
+		showList:showList,
+		setContent:setContent,
+		appendToHandle:appendToHandle,
+		createDroppable:createDroppable,
+		createDraggableItems:createDraggableItems,
+		makeArrayFromString:makeArrayFromString,
+		createNumberLine:createNumberLine,
+	};
+};
